01cf3a82ef52ff7ff0e89298d9881a631a3c7446
Alessio Carnevale <alessio.carnevale@gmail.com>
2019-08-29 15:00:01 +0200

    adding logo to htmlTemplate

--- htmlTemplate.js
+++ htmlTemplate.js
@@ -1,3 +1,5 @@
+const logo = require('./logo.js')
+
 module.exports = (html, ver) => {
   return `
     <!DOCTYPE html>
@@ -6,38 +8,50 @@ module.exports = (html, ver) => {
         <meta charset="utf-8" />
         <title>SVG - Inliner</title>    
         <style >
-          h1 {
-            font-size: 2rem;
+          a {
+            text-decoration: none;
+          }
+          pre {
+            font-size: 1.2rem;
+            color: #369;
           }
           body {
             font-family: monospace;
           }
           .icons-list {
             display:flex;
+            flex-wrap: wrap;
+            ustify-content: space-between;
           }
           .icon-def {
             display: flex;
             flex-direction: column;
             align-items: center;
-            margin: 10px;
+            margin: 20px;
           }
 
           .icon-name {
-            padding: 3px 5px;
-            background-color: #eee;
+            padding: 5px 5px;
+            color: #999;
+            width: 90px;
+            text-align: center;
           }
 
-          .icon-img svg {
-            width: 2rem;
-            height: 2rem;
+          .svg-icon {
+            width: 3rem;
+            height: 3rem;
+            color: #246;
           }
 
         </style>   
       </head>
       <body>
         <main>
-          <h1>SVG - Inliner</h1>
-          <p>ver. ${ver}</p>
+          <a href='https://github.com/cuginoAle/svg-inliner'>
+            <pre>
+              ${logo(ver)}
+            </pre>
+          </a>
           ${html}
         </main>
       </body>
